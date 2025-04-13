ea3c7df8dc9520532dbe9bb55858b9522f7499aa
iForged <147073106+iForged@users.noreply.github.com>
2025-04-13 07:16:10 +0300

    Update index.ts

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,7 +1,8 @@
 import { workerConfig } from '../../uptime.config'
-import { formatStatusChangeNotification, getWorkerLocation, notifyWithApprise } from './util'
+import { getWorkerLocation } from './util'
 import { MonitorState, MonitorTarget } from '../../uptime.types'
 import { getStatus } from './monitor'
+import { notifyDiscord } from './components/discord'
 
 export interface Env {
   UPTIMEFLARE_STATE: KVNamespace
@@ -11,42 +12,6 @@ export default {
   async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
     const workerLocation = (await getWorkerLocation()) || 'ERROR'
     console.log(`Running scheduled event on ${workerLocation}...`)
-
-    // Auxiliary function to format notification and send it via apprise
-    let formatAndNotify = async (
-      monitor: MonitorTarget,
-      isUp: boolean,
-      timeIncidentStart: number,
-      timeNow: number,
-      reason: string
-    ) => {
-      // Skip notification if monitor is in the skip list
-      // @ts-ignore
-      const skipList: string[] = workerConfig.notification?.skipNotificationIds
-      if (skipList && skipList.includes(monitor.id)) {
-        console.log(`Skipping notification for ${monitor.name} (${monitor.id} in skipNotificationIds)`)
-        return
-      }
-
-      if (workerConfig.notification?.appriseApiServer && workerConfig.notification?.recipientUrl) {
-        const notification = formatStatusChangeNotification(
-          monitor,
-          isUp,
-          timeIncidentStart,
-          timeNow,
-          reason,
-          workerConfig.notification?.timeZone ?? 'Etc/GMT'
-        )
-        await notifyWithApprise(
-          workerConfig.notification.appriseApiServer,
-          workerConfig.notification.recipientUrl,
-          notification.title,
-          notification.body
-        )
-      } else {
-        console.log(`Apprise API server or recipient URL not set, skipping apprise notification for ${monitor.name}`)
-      }
-    }
 
     // Read state, set init state if it doesn't exist
     let state =
@@ -68,7 +33,6 @@ export default {
     const currentTimeSecond = Math.round(Date.now() / 1000)
 
     // Check each monitor
-    // TODO: concurrent status check
     for (const monitor of workerConfig.monitors) {
       console.log(`[${workerLocation}] Checking ${monitor.name}...`)
 
@@ -103,14 +67,12 @@ export default {
         status = await getStatus(monitor)
       }
 
-      // const status = await getStatus(monitor)
       const currentTimeSecond = Math.round(Date.now() / 1000)
 
       // Update counters
       status.up ? state.overallUp++ : state.overallDown++
 
       // Update incidents
-      // Create a dummy incident to store the start time of the monitoring and simplify logic
       state.incident[monitor.id] = state.incident[monitor.id] || [
         {
           start: [currentTimeSecond],
@@ -118,50 +80,22 @@ export default {
           error: ['dummy'],
         },
       ]
-      // Then lastIncident here must not be undefined
       let lastIncident = state.incident[monitor.id].slice(-1)[0]
 
       if (status.up) {
         // Current status is up
-        // close existing incident if any
         if (lastIncident.end === undefined) {
           lastIncident.end = currentTimeSecond
           monitorStatusChanged = true
           try {
-            if (
-              // grace period not set OR ...
-              workerConfig.notification?.gracePeriod === undefined ||
-              // only when we have sent a notification for DOWN status, we will send a notification for UP status (within 30 seconds of possible drift)
-              currentTimeSecond - lastIncident.start[0] >= (workerConfig.notification.gracePeriod + 1) * 60 - 30
-            ) {
-              await formatAndNotify(
-                monitor,
-                true,
-                lastIncident.start[0],
-                currentTimeSecond,
-                'OK'
-              )
-            } else {
-              console.log(`grace period (${workerConfig.notification?.gracePeriod}m) not met, skipping apprise UP notification for ${monitor.name}`)
-            }
-
-            console.log('Calling config onStatusChange callback...')
-            await workerConfig.callbacks.onStatusChange(
-              env,
-              monitor,
-              true,
-              lastIncident.start[0],
-              currentTimeSecond,
-              'OK'
-            )
+            // Отправляем уведомление в Discord
+            await notifyDiscord(monitor, true)
           } catch (e) {
-            console.log('Error calling callback: ')
-            console.log(e)
+            console.log('Error sending Discord notification for UP status: ', e)
           }
         }
       } else {
         // Current status is down
-        // open new incident if not already open
         if (lastIncident.end !== undefined) {
           state.incident[monitor.id].push({
             start: [currentTimeSecond],
@@ -173,7 +107,6 @@ export default {
           lastIncident.end === undefined &&
           lastIncident.error.slice(-1)[0] !== status.err
         ) {
-          // append if the error message changes
           lastIncident.start.push(currentTimeSecond)
           lastIncident.error.push(status.err)
           monitorStatusChanged = true
@@ -181,67 +114,15 @@ export default {
 
         const currentIncident = state.incident[monitor.id].slice(-1)[0]
         try {
-          if (
-            // monitor status changed AND...
-            (monitorStatusChanged && (
-              // grace period not set OR ...
-              workerConfig.notification?.gracePeriod === undefined ||
-              // have sent a notification for DOWN status
-              currentTimeSecond - currentIncident.start[0] >= (workerConfig.notification.gracePeriod + 1) * 60 - 30
-            ))
-            ||
-            (
-              // grace period is set AND...
-              workerConfig.notification?.gracePeriod !== undefined &&
-              (
-                // grace period is met
-                currentTimeSecond - currentIncident.start[0] >= workerConfig.notification.gracePeriod * 60 - 30 &&
-                currentTimeSecond - currentIncident.start[0] < workerConfig.notification.gracePeriod * 60 + 30
-              )
-            )) {
-            await formatAndNotify(
-              monitor,
-              false,
-              currentIncident.start[0],
-              currentTimeSecond,
-              status.err
-            )
-          } else {
-            console.log(`Grace period (${workerConfig.notification?.gracePeriod}m) not met (currently down for ${currentTimeSecond - currentIncident.start[0]}s, changed ${monitorStatusChanged}), skipping apprise DOWN notification for ${monitor.name}`)
-          }
-
           if (monitorStatusChanged) {
-            console.log('Calling config onStatusChange callback...')
-            await workerConfig.callbacks.onStatusChange(
-              env,
-              monitor,
-              false,
-              currentIncident.start[0],
-              currentTimeSecond,
-              status.err
-            )
+            await notifyDiscord(monitor, false)
           }
         } catch (e) {
-          console.log('Error calling callback: ')
-          console.log(e)
-        }
-
-        try {
-          console.log('Calling config onIncident callback...')
-          await workerConfig.callbacks.onIncident(
-            env,
-            monitor,
-            currentIncident.start[0],
-            currentTimeSecond,
-            status.err
-          )
-        } catch (e) {
-          console.log('Error calling callback: ')
-          console.log(e)
+          console.log('Error sending Discord notification for DOWN status: ', e)
         }
       }
 
-      // append to latency data
+      // Append to latency data
       let latencyLists = state.latency[monitor.id] || {
         recent: [],
       }
@@ -254,13 +135,13 @@ export default {
       }
       latencyLists.recent.push(record)
 
-      // discard old data
+      // Discard old data
       while (latencyLists.recent[0]?.time < currentTimeSecond - 12 * 60 * 60) {
         latencyLists.recent.shift()
       }
       state.latency[monitor.id] = latencyLists
 
-      // discard old incidents
+      // Discard old incidents
       let incidentList = state.incident[monitor.id]
       while (incidentList.length > 0 && incidentList[0].end && incidentList[0].end < currentTimeSecond - 90 * 24 * 60 * 60) {
         incidentList.shift()
@@ -270,14 +151,11 @@ export default {
         incidentList[0].start[0] > currentTimeSecond - 90 * 24 * 60 * 60 &&
         incidentList[0].error[0] != 'dummy'
       )) {
-        // put the dummy incident back
-        incidentList.unshift(
-          {
-            start: [currentTimeSecond - 90 * 24 * 60 * 60],
-            end: currentTimeSecond - 90 * 24 * 60 * 60,
-            error: ['dummy'],
-          }
-        )
+        incidentList.unshift({
+          start: [currentTimeSecond - 90 * 24 * 60 * 60],
+          end: currentTimeSecond - 90 * 24 * 60 * 60,
+          error: ['dummy'],
+        })
       }
       state.incident[monitor.id] = incidentList
 
@@ -285,11 +163,9 @@ export default {
     }
 
     console.log(`statusChanged: ${statusChanged}, lastUpdate: ${state.lastUpdate}, currentTime: ${currentTimeSecond}`)
-    // Update state
-    // Allow for a cooldown period before writing to KV
     if (
       statusChanged ||
-      currentTimeSecond - state.lastUpdate >= workerConfig.kvWriteCooldownMinutes * 60 - 10  // Allow for 10 seconds of clock drift
+      currentTimeSecond - state.lastUpdate >= workerConfig.kvWriteCooldownMinutes * 60 - 10
     ) {
       console.log("Updating state...")
       state.lastUpdate = currentTimeSecond
